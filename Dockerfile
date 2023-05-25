b214696855cbf92605bc190efe21aafb2eda4d2a
cristobal <cpadillar1995@hotmail.com>
2023-05-24 22:35:06 -0500

    modificacion de archivos de docker para resolver problema de conexion

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,22 @@
+# Utiliza una imagen base de Python
 FROM python:3.9
 
-WORKDIR /app
+# Establece el directorio de trabajo
+WORKDIR /src
 
+# Establece el archivo requirements.txt
 COPY requirements.txt .
 
+# Instala las dependencias del proyecto
 RUN pip install --no-cache-dir -r requirements.txt
 
+# Copia el codigo base del proyecto
+COPY /app ./app
+COPY run.py .
+
+# Expone el puerto 5000 (puerto predeterminado para Flask)
 EXPOSE 5000
 
 ENV PYTHONHTTPSVERIFY=0
 
-CMD ["python", "main.py"]
+CMD ["python", "run.py", "--host=0.0.0.0"]
--- app/database.py
+++ app/database.py
@@ -1,11 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 
 db = SQLAlchemy()
+db_host = os.environ.get('DB_HOST', 'localhost')
+db_port = os.environ.get('DB_PORT', '5432')
+db_name = os.environ.get('DB_NAME', 'mydatabase')
+db_user = os.environ.get('DB_USER', 'myuser')
+db_password = os.environ.get('DB_PASSWORD', 'mypassword')
 
 def configure_database(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:loquillo1995@127.0.0.1:5432/prueba'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}'
     db.init_app(app)
     with app.app_context():
         db.create_all()
